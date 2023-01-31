002dcb60c4667633dfe4fdaf84868c90c5edc5f2
Ntclmx <69081326+Ntclmx@users.noreply.github.com>
2023-02-01 00:44:44 +0700

    api planning, route api and error
    planning api kedepannya, routing api untuk event, dan catch error

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const eventRoutes = require('./src/routes/event');
+const app = express();
 
-const event = require('./src/routes/event');x
+const PORT = 4000;
 
-app.use('/', event);
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+app.use('/v1/', eventRoutes);
 
 app.use((req, res) => {
     res.status(404);
     res.send('404 Not Found');
 });
 
-app.listen(port, () => {
-    console.log(`Tibloc running on http://localhost:${port}`);
+app.use((error, req, res, next) => {
+    const status = error.errorStatus || 500;
+    const message = error.message;
+    const data = error.data;
+
+    res.status(status).json({ message : message, data: data});
+});
+
+app.listen(PORT, () => {
+    console.log(`Tibloc running on http://localhost:${PORT}`);
 });
--- back-end/src/routes/event.js
+++ back-end/src/routes/event.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const app = express();
+const { body } = require('express-validator');
 
+const router = express.Router();
 const eventController = require('../controllers/event');
 
-app.get('/event', eventController.getEvents);
 
-app.post('/event', eventController.postEvents);
+router.post('/event', [
+    body('eventTitle').isLength({min:5}).withMessage('Event Title tidak sesuai'),
+    body('eventTnc').isLength({min:5}).withMessage('Event TNC tidak sesuai'),
+    ],
+    eventController.postEvent);
 
-module.exports = app;
+router.get('/event', eventController.getEvent);
+
+module.exports = router;
