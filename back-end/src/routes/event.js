e04d9b86321a4e3a778af056ea47afff07daabe0
Ntclmx <69081326+Ntclmx@users.noreply.github.com>
2023-02-04 14:47:42 +0700

    list event update
    fe: event card
    be: curd event (pagination)

--- back-end/src/routes/event.js
+++ back-end/src/routes/event.js
@@ -6,11 +6,20 @@ const eventController = require('../controllers/event');
 
 
 router.post('/event', [
-    body('eventTitle').isLength({min:5}).withMessage('Event Title tidak sesuai'),
-    body('eventTnc').isLength({min:5}).withMessage('Event TNC tidak sesuai'),
+        body('eventTitle').isLength({min:5}).withMessage('Event Title tidak sesuai'),
+        body('eventTnc').isLength({min:5}).withMessage('Event TNC tidak sesuai'),
     ],
     eventController.postEvent);
 
-router.get('/event', eventController.getEvent);
+router.get('/events', eventController.getAllEvents);
+
+router.get('/event/:eventId', eventController.getEvent);
+
+router.put('/event/:eventId', [
+    body('eventTitle').isLength({min:5}).withMessage('Event Title tidak sesuai'),
+    body('eventTnc').isLength({min:5}).withMessage('Event TNC tidak sesuai'),
+],eventController.updateEvent);
+
+router.delete('/event/:eventId', eventController.deleteEvent);
 
 module.exports = router;
