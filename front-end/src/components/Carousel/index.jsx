2f570111198566dc2257fa8cbd86f1c640499b58
Ntclmx <69081326+Ntclmx@users.noreply.github.com>
2023-02-11 14:01:24 +0700

    update home

--- front-end/src/components/Carousel/index.jsx
+++ front-end/src/components/Carousel/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-// import {Carousel} from 'react-bootstrap/carousel'
-
+import Carousel from 'react-bootstrap/Carousel';
+import CarouselImage from '../../assets/events/event1.jpg'
+import './carousel.css'
 // function checkIndex(banner){
 //   if(banner.index === 1){
 //     return (
@@ -18,67 +19,47 @@ import React from "react";
 // }
 
 const CarouselTibloc = () => {
-  // const banners = props.banners;
-  // const carouselBody = banners.map((banner) => 
-  //   checkIndex(banner)
-  // )
+
   return (
-    // <div
-    //   id="carouselExample"
-    //   class="carousel slide"
-    // >
-    //   <div class="carousel-inner">
-    //     {/* {carouselBody} */}
-    //     <div class="carousel-item active">
-    //       <img src="https://cdn1-production-images-kly.akamaized.net/WtfFC1e1Pitdvxc8iQykfJqDkuA=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3332604/original/036907800_1608786362-WhatsApp_Image_2020-12-24_at_07.58.39.jpeg" class="d-block w-50 h-50" alt="foto1" />
-    //     </div>
-    //     <div class="carousel-item">
-    //       <img src="https://images.bisnis.com/posts/2021/09/29/1448535/kue-subuh-1.jpg" class="d-block w-80 h-50" alt="foto2" />
-    //     </div>
-    //   </div>
-    //   <button
-    //     class="carousel-control-prev"
-    //     type="button"
-    //     data-bs-target="#carouselExample"
-    //     data-bs-slide="prev"
-    //   >
-    //     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    //     <span class="visually-hidden">Previous</span>
-    //   </button>
-    //   <button
-    //     class="carousel-control-next"
-    //     type="button"
-    //     data-bs-target="#carouselExample"
-    //     data-bs-slide="next"
-    //   >
-    //     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    //     <span class="visually-hidden">Next</span>
-    //   </button>
-    // </div>
-    <>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://cdn1-production-images-kly.akamaized.net/WtfFC1e1Pitdvxc8iQykfJqDkuA=/640x360/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3332604/original/036907800_1608786362-WhatsApp_Image_2020-12-24_at_07.58.39.jpeg" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="https://images.bisnis.com/posts/2021/09/29/1448535/kue-subuh-1.jpg" class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src="https://i.ytimg.com/vi/Zmi-Hd0qyT8/maxresdefault.jpg" class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  
-</div>
-    </>
+    <Carousel className="main-carousel">
+      <Carousel.Item>
+        <img
+          className="d-block carousel-image"
+          src={CarouselImage}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block carousel-image"
+          src={CarouselImage}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block carousel-image"
+          src={CarouselImage}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
